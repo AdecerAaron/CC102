cc0cbb34b06c81b48ee4feaa949c0c4265799b8b
ian rey <adeceraaron24@gmail.com>
2024-11-25 09:29:51 +0800

    Add files via upload

--- Movie.cpp
+++ Movie.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main() {
 	int age;
-	char answer, YnO;
+	float answer;
+	char  YnO;
 	cout << "Enter your age ";
 	cin >> age;
 	if (age < 13) {
@@ -14,59 +15,51 @@ int main() {
 			cout << "rated G,PG movies" << endl;	
 		}else {
 			cout << "Invalid Output";
-			
 		}
-		cout << "Enter time of show [M = Matinne, E = Evening] ";
+		cout << "Enter amount [Matinee = 7.50 | Evening = 10.50] ";
 			cin >> answer;
-			if (answer == 'M'){
-			cout << "Amount = 7.50";
-		} else if (answer == 'E'){
-			cout << "Amount = 10.50";
+			if (answer == 7.50){
+			cout << "Can Go to Matinee show ";
+		} else if (answer == 10.50){
+			cout << "Can Go to Evening show";
+		} else if (answer < 7.50){
+			cout << "Not enough amount ";
 		} else {
-			cout << "invalid Output";
+			cout << "Invalid Input " ;
 		}
-		
-	}else if (age >= 13 && age < 16){
-		cout << "rated G,PG Movies" << endl;
-		cout << "Enter time of show [M = Matinne, E = Evening] ";
-		cin >> answer;
-			if (answer == 'M'){
-			cout << "Amount = 7.50";
-		} else if (answer == 'E'){
-			cout << "Amount = 10.50";
-		} else {
-			cout << "invalid Output";
-		}
-	}else if (age < 16) {
+	}else if (age > 13 && age < 16){
 		cout << "with parent?[y = YES, n = NO] ",
 		cin >> YnO;
 		if (YnO == 'n'){
-			cout << "rated G,PG,R movies" << endl;
+			cout << "rated G,PG movies" << endl;
 		}else if (YnO == 'y'){
-			cout << "rated G,PG, movies" << endl;	
+			cout << "rated G,PG,R movies" << endl;	
 		}else {
 			cout << "Invalid Output";
-			
 		}
-		cout << "Enter time of show [M = Matinne, E = Evening] ";
+		cout << "Enter amount [Matinee = 7.50 | Evening = 10.50] ";
 			cin >> answer;
-			if (answer == 'M'){
-			cout << "Amount = 7.50";
-		} else if (answer == 'E'){
-			cout << "Amount = 10.50";
+			if (answer == 7.50){
+			cout << "Can Go to Matinee show ";
+		} else if (answer == 10.50){
+			cout << "Can Go to Evening show";
+		} else if (answer < 7.50){
+			cout << "Not enough amount ";
 		} else {
-			cout << "invalid Output";
+			cout << "Invalid Input ";
 		}
-	}else if (age > 16){
+	}else if (age >= 16){
 		cout << "rated G,PG,R Movies" << endl;
-		cout << "Enter time of show [M = Matinne, E = Evening] ";
-		cin >> answer;
-			if (answer == 'M'){
-			cout << "Amount = 7.50";
-		} else if (answer == 'E'){
-			cout << "Amount = 10.50";
+		cout << "Enter amount [Matinee = 7.50 | Evening = 10.50] ";
+			cin >> answer;
+			if (answer == 7.50){
+			cout << "Can Go to Matinee show ";
+		} else if (answer == 10.50){
+			cout << "Can Go to Evening show";
+		} else if (answer < 7.50){
+			cout << "Not enough amount ";
 		} else {
-			cout << "invalid Output";
+			cout << "Invalid Input ";
 		}
 	}
 return 0;	

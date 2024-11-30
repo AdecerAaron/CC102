f206e2eafe01c22c195a0fd3be0d87862b62346c
ian rey <adeceraaron24@gmail.com>
2024-12-01 01:32:29 +0800

    Add files via upload

---
+++ Looping-Activity-G.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+	char symbol;
+	cout << "Enter symbol: ";
+	cin << Symbol;
+	for (int i = 0; i < 5; i++) {
+		if (i % 2 == 1) {
+			cout << " ";
+		}
+		for (int j = 0; j < 7; j++){
+			cout << symbol << " ";
+		}
+		cout << endl;
+	}
+	return 0 ;
+}
